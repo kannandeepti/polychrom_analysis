af0af639840927ca535d140dc99312de148f873e
Deepti Kannan <kannandeepti@yahoo.com>
2023-10-18 14:05:29 -0400

    BUGFIX: combined snapshot/animation visualization code
    CODE: script to create a clean conda environment for analysis and visualization

---
+++ install_visualization.sh
@@ -0,0 +1,21 @@
+!/bin/bash
+""" Script to create a clean conda environment solely for the
+purposes of visualizing polychrom snapshots with NGLview
+and post-processing analysis. 
+
+Why is this separate from the `polychrom` environment?
+- due to version incompatibilities with openmm
+- TODO: figure out an all encompasing environment for simulations + analysis
+
+"""
+
+conda create -n polyvis python=3.9 matplitlib numpy scipy jupyter ipython pandas h5py joblib seaborn numba
+conda activate polyvis
+pip install -U git+https://github.com/open2c/polychrom
+conda install nglview -c conda-forge
+pip install -U git+https://github.com/mirnylab/nglutils
+python -m ipykernel install --sys-prefix
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+jupyter nbextension enable --py --sys-prefix nglview
+python -m ipykernel install --user --name=polyvis
+pip install -U git+https://github.com/open2c/cooltools
